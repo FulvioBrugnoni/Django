c4091a173ba5db4baaa21560e2423bb678722ba1
Fulvio <fulvio@MacBook-Pro-di-Fulvio.local>
2020-10-05 10:24:14 +0200

    quarto

--- vr/questionario/models.py
+++ vr/questionario/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Questionario(models.Model):
-    nome = name.Chardfield(max_lenght =256)
+    name = models.CharField(max_length=256)
 
     class Meta:
             db_table ='questionario'
             verbose_name ='questionario'
             verbose_name_plural ='questionari'
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
