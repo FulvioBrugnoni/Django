e4a33b0c7b2d46c47e9adb83f4729ee3dda4d61d
Fulvio <fulvio@MacBook-Pro-di-Fulvio.local>
2020-10-05 10:30:02 +0200

    quinto

--- vr/questionario/models.py
+++ vr/questionario/models.py
@@ -10,3 +10,16 @@ class Questionario(models.Model):
             verbose_name_plural ='questionari'
     def __str__(self):
         return self.name
+
+
+class Domanda(models.Model):
+
+    chiave= models.CharField(max_length=6)
+    testo = models.CharField(max_length=256)
+
+    class Meta:
+            db_table ='domanda'
+            verbose_name ='domanda'
+            verbose_name_plural ='domande'
+    def __str__(self):
+        return self.chiave
